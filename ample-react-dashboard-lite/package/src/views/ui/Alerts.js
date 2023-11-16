04deef6714d2ddd7cfeea6a8f00b5b60d9bb01d0
programmingwbn <computergwbn@gmail.com>
2023-11-16 00:08:37 -0800

    dropdown feature without the backend server
    dropdown feature without the backend server

--- ample-react-dashboard-lite/package/src/views/ui/Alerts.js
+++ ample-react-dashboard-lite/package/src/views/ui/Alerts.js
@@ -7,11 +7,74 @@ import {
   CardTitle,
 } from "reactstrap";
 // import record from "./test.json";
+import VariantsExample from "./Dropdown";
+import { Dropdown, DropdownToggle, DropdownMenu } from "react-bootstrap";
+import { startOfMonth, addWeeks, isSunday, format, isAfter, isToday, startOfWeek, subWeeks, eachDayOfInterval } from 'date-fns';
+
 
 const Companyone = () => {
   // For Dismiss Button with Alert
   const [visible, setVisible] = useState(true);
   const [record, setRecord] = useState([]);
+
+  const [date, setdate] = useState("");
+  const [aimodel, setaimodel] = useState("");
+
+
+  const [options, setOptions] = useState([
+    {
+      text: "OpenAI",
+      value: "OpenAI",
+    },{
+      text: "Ai Tech 1",
+      value: "Ai tech 1",
+    },
+    {
+      text: "Aitech 2 2",
+      value: "Aitech 2 2",
+    },
+    {
+      text: "Aitech 3 2",
+      value: "Aitech 2 2",
+    },
+  ]);
+
+  const handleChangedate = (e) => {
+    setdate(e.value)
+    };
+
+    const handleChangeai = (e) => {
+      setaimodel(e.value)
+      };
+
+      function getLatestFiveSundays() {
+          // Get the current date
+          const currentDate = new Date();
+        
+          // Find the most recent Sunday
+          const lastSunday = startOfWeek(currentDate);
+        
+          // Generate an array of dates for the latest five Sundays
+          const latestFiveSundays = eachDayOfInterval({
+            start: subWeeks(lastSunday, 4),  // Subtract 4 weeks to get the latest five Sundays
+            end: lastSunday,
+          });
+        
+          // Convert dates to string format
+          const dateStrings = latestFiveSundays.map(date => format(date, 'yyyy-MM-dd'));
+        
+          console.log(dateStrings);
+
+          const dateStrings2 = dateStrings.map((text, index) => ({
+            text,
+            value: text,
+          }));
+
+          return dateStrings2;
+        }
+const [days,setdays] = useState (getLatestFiveSundays())
+
+
 
   // const onMount = async () => {
   //   try {
@@ -28,8 +91,8 @@ const Companyone = () => {
   // };
   const onMount = async () => {
     try {
-      const date = '08-11-2023'; // Replace with the desired date value
-      const model = 'OpenAI'; // Replace with the desired model value
+      // const date = '08-11-2023'; // Replace with the desired date value
+      // const model = 'OpenAI'; // Replace with the desired model value
   
       const requestOptions = {
         method: 'POST',
@@ -38,7 +101,7 @@ const Companyone = () => {
         },
         body: JSON.stringify({
           date,
-          model,
+          aimodel,
         }),
       };
   
@@ -59,6 +122,11 @@ const Companyone = () => {
   useEffect(() => {
     onMount(); // Call the onMount function when the component is mounted
   }, []); // The empty dependency array ensures it runs only once on mount
+  
+  useEffect(() => {
+    onMount();
+  }, [date, aimodel]);
+
   const onDismiss = () => {
     setVisible(false);
   };
@@ -69,6 +137,9 @@ const Companyone = () => {
       {/* Card-1*/}
       {/* --------------------------------------------------------------------------------*/}
 
+
+
+ 
       <CardTitle tag="h6" className="border-bottom p-3 mb-0">
         <i className="bi bi-bell me-2"> </i>
         {
@@ -112,6 +183,29 @@ const Companyone = () => {
         <CardTitle tag="h6" className="border-bottom p-3 mb-0">
           <i className="bi bi-bell me-2" />
           company summary #2
+          <div>
+      <Dropdown onselect={handleChangedate}>
+        <DropdownToggle variant="primary">Select Date</DropdownToggle>
+        <DropdownMenu>
+          {days.map((option) => (
+            <Dropdown.Item key={option.value} onClick={() => handleChangedate(option)}>{option.text}</Dropdown.Item>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+
+      <Dropdown>
+        <DropdownToggle variant="primary">Select AI</DropdownToggle>
+        <DropdownMenu>
+          {options.map((option) => (
+            <Dropdown.Item key={option.value} onClick={() => handleChangeai(option)}>{option.text}</Dropdown.Item>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+
+    <div>
+      <> showing data for {date} and aitechnology {aimodel}</>
+    </div>
         </CardTitle>
         <CardBody className="">
           <div>
